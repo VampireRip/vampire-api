66dc6e6e4f03afe6e50896ffc9177afa0c48a36f
SunriseFox <sunrisefox@vampire.rip>
2018-12-21 15:08:05 +0800

    now it works under nginx

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const dir = require('./dir')
-// const proxy = require('http-proxy-middleware')
 
 const app = express()
 
 app.use(logger('dev'))
-app.use((req, res, next) => {
-  if (req.hostname === 'os.vampire.rip') {
-    return express.static(path.resolve(dir.os, 'public'))(req, res, next)
-  }
-  return next()
-})
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use('/github-webhook/vampire-os', require('./webhook/vampire-os'))
 app.use('/github-webhook/vampire', require('./webhook/vampire'))
-app.use(express.static(path.resolve(dir.vampire, 'public')))
 
 app.use((req, res, next) => {
   if (res.headersSent) return
--- dir.js
+++ dir.js
@@ -1,12 +1,16 @@
 const path = require('path')
 const isWindows = process.platform === 'win32'
 
+const host = {
+  vampire: 'vampire.rip',
+  os: 'os.vampire.rip',
+  api: 'api.vampire.rip'
+}
+
 module.exports = isWindows ? {
-  vampire: path.resolve(__dirname, '../vampire'),
-  os: path.resolve(__dirname, '../vampire-os'),
-  cert: path.resolve(__dirname, '../cert')
+  vampire: path.resolve(__dirname, '../', host.vampire),
+  os: path.resolve(__dirname, '../', host.os)
 } : {
-  vampire: '/var/www/vampire',
-  os: '/var/www/vampire-os',
-  cert: '/var/www/cert'
+  vampire: path.join('/var/www/', host.vampire),
+  os: path.join('/var/www/vampire-os', host.os)
 }
